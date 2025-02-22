0073c25296424cf1bf44fc57614cbf31d1f18fe8
Rishab Chavadar <rchavadar@gmail.com>
2025-02-22 12:12:06 +0530

    Hello World.cpp

--- Hello World Program.cpp
+++ Hello World Program.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
-
-int main() 
-{
-    int favorite_number;
-    
-    std::cout << "Enter your favorite number between 1 and 100: ";
-    std::cin >> favorite_number;
-    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
-    
+int main(){
+    std::cout << "Hello World" << std::endl;
     return 0;
 }
+
+// #include <iostream>
+// int main() 
+// {
+//     int favorite_number;
+//     std::cout << "Enter your favorite number between 1 and 100: ";
+//     std::cin >> favorite_number;
+//     std::cout << "Amazing!! That's my favorite number too!" << std::endl;    
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     cout << "Hello World" << endl;
+//     return 0;
+// }
